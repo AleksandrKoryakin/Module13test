1b8007c10b3d752eb0634c9e666f996d83021321
AleksandrKoryakin <88359979+AleksandrKoryakin@users.noreply.github.com>
2022-06-03 11:23:56 +0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,9 @@
+let swiper = new Swiper('.swiper', {
+  slidesPerView: 1,
+  slidesPerGroup: 1,
+  pagination: {
+    el: '.swiper-pagination',
+    type: 'bullets',
+    clickable: true,
+  },
+});
